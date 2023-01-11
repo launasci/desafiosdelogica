b3f0b5d8b0abb9c755e4a58d4102ba063c9573d2
lauranascimento <laura.nascimento@buser.com.br>
2023-01-11 12:14:04 -0300

    leet-code

---
+++ leet-code/parenteses-validos.js
@@ -0,0 +1,56 @@
+// Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+// An input string is valid if:
+
+// Open brackets must be closed by the same type of brackets.
+// Open brackets must be closed in the correct order.
+// Every close bracket has a corresponding open bracket of the same type.
+ 
+
+// Example 1:
+
+// Input: s = "()"
+// Output: true
+// Example 2:
+
+// Input: s = "()[]{}"
+// Output: true
+// Example 3:
+
+// Input: s = "(]"
+// Output: false
+ 
+
+// Constraints:
+
+// 1 <= s.length <= 104
+// s consists of parentheses only '()[]{}'.
+
+// SOLUÇÃO
+
+// var isValid = function (s){
+//   const entradasValidas = '()[]{}'
+//   if(entradasValidas.includes(s)) return true
+//   return false
+// }
+
+var isValid = function(s) {
+   let entradasValidas = {
+        "(" : ")",
+        "{" : "}",
+        "[" : "]"
+    };
+    let listaDeEntradas = new Array();
+    for(let entrada = 0; entrada <s.length; entrada++){
+
+        if(s.charAt(entrada) === entradasValidas[listaDeEntradas[listaDeEntradas.length-1]]){
+            listaDeEntradas.pop();
+        }
+        else{
+            listaDeEntradas.push(s.charAt(entrada));
+        }
+    }
+    return (listaDeEntradas.length == 0);
+};
+
+console.log(isValid('{[]}'))
